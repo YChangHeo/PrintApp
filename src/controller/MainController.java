36f7e1067f866f71dce450f634c24db42511b1a5
SONY <SONY@SONY-VAIO.lan>
2018-03-24 09:41:47 +0700

    chien 24/03/2018

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -3,11 +3,13 @@
  */
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+
 /**
  * @author Lam.Nguyen
  * @since 2018/03/10
  * @version 1.0
  */
 public class MainController {
-	
+	private JFXButton adminBtn;
 }
